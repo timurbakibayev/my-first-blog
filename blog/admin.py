934cb76516cd2eef5304b89e704ae881cd033c6f
Timur Bakibayev x1 <timurbakibayev@gmail.com>
2022-03-31 13:51:54 +0200

    items and carts

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,16 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Cart, Item, CartItem
+
+
+class CartItemAdmin(admin.TabularInline):
+    model = CartItem
+
+
+class CartAdmin(admin.ModelAdmin):
+   inlines = [CartItemAdmin,]
+
 
 admin.site.register(Post)
+admin.site.register(Cart, CartAdmin)
+admin.site.register(Item)
+admin.site.register(CartItem)
--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 class Post(models.Model):
@@ -16,3 +17,35 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=1000)
+    price = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.name}, {self.price} KZT"
+
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def total(self):
+        cart_items = CartItem.objects.filter(cart=self)
+        s = 0
+        for cart_item in cart_items:
+            s = s + cart_item.item.price * cart_item.count
+
+        return s
+
+    def __str__(self):
+        return f"Cart of {self.user.username}, total: {self.total()} KZT"
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.DO_NOTHING)
+    item = models.ForeignKey(Item, on_delete=models.DO_NOTHING)
+    count = models.IntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.item}, {self.count} pc"
